5b6194f2f249f0b1c223d90e3fb65fa1bf54808f
Ankit-Patra <ankitpatra0123@gmail.com>
2023-11-10 21:41:23 +0530

    added finish line

---
+++ SnowBoarder/Assets/Script/FinishLine.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("You Finished");
+        }
+    }
+}
